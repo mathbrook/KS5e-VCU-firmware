870a731b883eb32675c3904f0bb30997e1c30690
Ben Hall <rcmast3r1@gmail.com>
2022-07-03 01:07:43 -0400

    cleaning up

--- include/ADC_SPI.h
+++ include/ADC_SPI.h
@@ -7,18 +7,19 @@
 #define DEFAULT_SPI_CS 10
 #define DEFAULT_SPI_SPEED 1000000
 
-class ADC_SPI {
-	public:
-		ADC_SPI();
-		ADC_SPI(int CS);
-        ADC_SPI(int CS, unsigned int SPIspeed);
-		void init(int CS, unsigned int SPIspeed);
-		uint16_t read_adc(int channel);
-	private:
-		int ADC_SPI_CS;
-        unsigned int SPI_SPEED;
-};
+class ADC_SPI
+{
+public:
+    ADC_SPI();
+    ADC_SPI(int CS);
+    ADC_SPI(int CS, unsigned int SPIspeed);
+    void init(int CS, unsigned int SPIspeed);
+    uint16_t read_adc(int channel);
 
+private:
+    int ADC_SPI_CS;
+    unsigned int SPI_SPEED;
+};
 
 #endif // !<ADC_SPI.H>
 #pragma once
