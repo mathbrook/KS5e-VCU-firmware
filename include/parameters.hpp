3d092e822080500602025a39276b4e40f08a9b80
mathbrook <32876429+mathbrook@users.noreply.github.com>
2022-11-04 20:29:23 -0400

    PID tuned after test night 1

--- include/parameters.hpp
+++ include/parameters.hpp
@@ -9,14 +9,14 @@
 
 //CRUISE CONTROL
 #define SLIP 1.1
-#define SET_RPM 1000
-#define D_KP 4.1
-#define D_KI 0
-#define D_KD 0
+#define SET_RPM 1624
+#define D_KP 1.5
+#define D_KI 0.3
+#define D_KD 0.5
 #define D_OUTPUT_MIN 0.0
-#define D_OUTPUT_MAX (TORQUE_1 * 10)
+#define D_OUTPUT_MAX (TORQUE_1*10)
 #define BANGBANG_RANGE 1000.0
-#define PID_TIMESTEP 10.0
+#define PID_TIMESTEP 100.0
 #define PID_MODE false
 #define PID_TC_MODE false
 //#define EXP_TORQUE_CURVE 
@@ -44,8 +44,8 @@
 // #define HT_DEBUG_EN
 // Torque Calculation Defines
 #define ALPHA 0.9772
-#define TORQUE_1 160
-#define TORQUE_2 160
+#define TORQUE_1 180
+#define TORQUE_2 180
 // Pump speed
 #define PUMP_SPEED 3400
 
