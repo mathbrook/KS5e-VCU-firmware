f1b5f119fe694d4bd0edcc2ddca684a8041dbb55
LforLovey <93688568+LforLovey@users.noreply.github.com>
2023-04-08 01:49:24 -0400

    running during test, fixed apps

--- src/pedal_handler.cpp
+++ src/pedal_handler.cpp
@@ -57,11 +57,12 @@ bool PedalHandler::read_pedal_values()
     steering_angle_ = pedal_ADC.read_adc(3);
 
     
-    // Serial.print("ADC1 :");
-    // Serial.println(accel1_);
-    // Serial.print("ADC2 :");
-    // Serial.println(accel2_);
-
+    Serial.print("ADC1 :");
+    Serial.println(accel1_);
+    Serial.print("ADC2 :");
+    Serial.println(accel2_);
+    Serial.print("BRAKE :");
+    Serial.println(brake1_);
 
 
     // This is the code to print raw ADC readings vs the filtered one
