4963f2696bdcc26af155cd56261e52c3d61f57fe
jstri114 <jstri114@students.kennesaw.edu>
2023-06-16 13:05:37 -0400

    Calibrated pedal & 120nm

--- include/parameters.hpp
+++ include/parameters.hpp
@@ -31,12 +31,12 @@
 #define BRAKE_ACTIVE 2650             // Threshold for brake pedal active
 #define MIN_ACCELERATOR_PEDAL_1 0    // Low accelerator implausibility threshold
 #define START_ACCELERATOR_PEDAL_1 50  // Position to start acceleration
-#define END_ACCELERATOR_PEDAL_1 2300    // Position to max out acceleration
-#define MAX_ACCELERATOR_PEDAL_1 2500    // High accelerator implausibility threshold
+#define END_ACCELERATOR_PEDAL_1 1700    // Position to max out acceleration
+#define MAX_ACCELERATOR_PEDAL_1 2000    // High accelerator implausibility threshold
 #define MIN_ACCELERATOR_PEDAL_2 0   // Low accelerator implausibility threshold
 #define START_ACCELERATOR_PEDAL_2 50  // Position to start acceleration
-#define END_ACCELERATOR_PEDAL_2 3000    // Position to max out acceleration
-#define MAX_ACCELERATOR_PEDAL_2 3200    // High accelerator implausibility threshold
+#define END_ACCELERATOR_PEDAL_2 2700    // Position to max out acceleration
+#define MAX_ACCELERATOR_PEDAL_2 3000    // High accelerator implausibility threshold
 #define HALF_ACCELERATOR_PEDAL_1 ((START_ACCELERATOR_PEDAL_1 + END_ACCELERATOR_PEDAL_1) / 2)
 #define HALF_ACCELERATOR_PEDAL_2 ((START_ACCELERATOR_PEDAL_2 + END_ACCELERATOR_PEDAL_2) / 2)
 #define ALPHA 0.9772
